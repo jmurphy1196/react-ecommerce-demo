267cd2d055086d1c5c03d63ef21fab3cd27be319
jason murphy <jmurphy.1196@gmail.com>
2020-07-17 06:06:33 -0700

    navbar setup

--- ecommerce-demo/src/App.js
+++ ecommerce-demo/src/App.js
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar';
 import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Cart from './components/Cart';
 import Default from './components/Defualt';
 import Details from './components/Details';
 import ProductList from './components/ProductList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
--- ecommerce-demo/src/components/Navbar.js
+++ ecommerce-demo/src/components/Navbar.js
@@ -1,6 +1,10 @@
 
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import logo from '../store.svg';
+import styled from 'styled-components';
+import ButtonContainer from './Button';
+
 
 
 export default class Navbar extends Component {
@@ -15,11 +19,34 @@ export default class Navbar extends Component {
 
 
         return(
-            <div>
-                hello from navbar
+            <div className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
+                <Link to='/'>
+                
+                    <img src={logo} alt='store' className='navbar-brand' width='60' />
+
+                </Link>
+
+                <ul className='navbar-nav align-items-center'>
+                    <li className='nav-item ml-5'>
+                        <Link to='/' className='nav-link'>
+                            Products
+                        </Link>
+                        
+                    </li>
+                  
+                </ul>
+                <Link  to='/cart' className='ml-auto' className='ml-auto'> 
+                    <ButtonContainer >
+                       <span className='mr-2'>
+                       <i className='fas fa-cart-plus'></i> cart
+                           
+                       </span>
+                    </ButtonContainer> 
+               </Link>
             </div>
         )
     }
 
 
 }
+
